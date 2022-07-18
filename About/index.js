3182c3fe27a676ce5c9e6370cef8d4bde78d5312
Nirmal <89763163+nirdah@users.noreply.github.com>
2022-07-18 03:50:17 -0500

    Add files via upload

---
+++ About/index.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import './index.css';
+
+function About() {
+    return (
+      <div>
+
+      <div class="wrapper">
+        <div class="hr">
+          <hr/>
+          <h2>Our Mission</h2>
+          <hr/>
+        </div>
+  
+        <p class="mission-txt">"To Deliver Quality Jersey Products at Low Cost"</p>
+        <p class="mission-txt">"Our goal is to provide the best personalized athletic apparel available in the market, deliver our goods 
+        on schedule, and charge reasonable pricing for them when we deal with clients directly.</p>
+      </div>
+  <br></br>
+      <div class="wrapper">
+        <div>
+          <img src="https://heartofgodisrael.org/wp-content/uploads/2012/02/goal2.png" alt="img"/>
+          <br></br> <br></br>
+          <p>For every person, participating in a sporting event or donning a jersey is highly important. Additionally, 
+            
+            Jersey should be comfy to wear in any circumstance while supporting or participating. Therefore, our organization 
+            
+            produces high-quality Jersey for both men and women. For the comfort and fitness, we take care of every specific size
+            
+             for men and women. Additionally, we personalize each customer's name and number on their Jersey by using their preferred 
+             
+             writing style. Because of their poor quality, most jerseys are unwearable in hot weather, but ours are made of high-quality
+             
+              material that keeps you cool and comfortable. Jersey should have a stylish appearance, so our company designed it uniquely.
+              
+              Sports jerseys are the uniform that a team wears for a sports match. Every team has a different jersey than the other teams.</p>
+<br></br>
+<p>The jersey is designed specifically based on the team, the name of the team the city or country the team is representing.
+
+If the team is representing a country then the jersey will be designed in such a way that it matches the flag of the country.
+
+For example, the jersey usually matches the color of the flag as well as the name of the country team is representing.
+
+The jersey for every sport is made from a different fabric depending upon the nature of the sport. For example, the ice-skating jersey is made of parachute jackets 
+
+while the volleyball jersey is of thin fabric so that players can move freely.</p>
+<br></br>
+          <p>Now lets move to shipping and delivery information. We provide free delivery to our costumers with fastest shipping available. 
+            We try our best to provide quick and quality costumer service. Our company have quick refund and exchange policy as well if you
+             got the damaged product or got the wrong product delivered. 
+<br></br>
+<br></br>
+<h3>What makes KTM Customized jersey unique?</h3>
+We are continuously working to provide our select group of clients with the most recent in design, manufacturing, quality, and 
+service because we know what makes a great sports uniform. We exclusively utilize professional-grade materials. We are able to 
+do this thanks to having our own factory, solid partnerships with suppliers of premium sports uniform materials, and business partners.
+</p>
+  
+        </div>
+      </div>
+  
+    </div>
+    )
+}
+
+export default About
